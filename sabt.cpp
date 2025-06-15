7265d279713bf3389c91e4e3cacac7f7593e3f0b
SabalThapa3 <sabalthapa5@gmail.com>
2025-06-15 17:33:53 +0545

    Update sabt.cpp

--- sabt.cpp
+++ sabt.cpp
@@ -1,38 +1,50 @@
 #include <iostream>
 using namespace std;
 
-char board[3][3]; // 3x3 game board
+char board[3][3]; 
 
-void initializeBoard() {
+void initializeBoard()
+{
     char num = '1';
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < 3; i++)
+        {
+        for (int j = 0; j < 3; j++) 
+        {
             board[i][j] = num++;
         }
     }
 }
 
-void printBoard() {
+void printBoard() 
+{
     cout << endl;
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++) 
+    {
         cout << " ";
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < 3; j++) 
+        {
             cout << board[i][j];
             if (j < 2) cout << " | ";
         }
         cout << endl;
-        if (i < 2) cout << "---|---|---" << endl;
+        if (i < 2) 
+        {
+            cout << "---|---|---" << endl;
+        }
     }
     cout << endl;
 }
 
-bool placeMark(int choice, char mark) {
+bool placeMark(int choice, char mark) 
+{
     int row = (choice - 1) / 3;
     int col = (choice - 1) % 3;
     
-    if (choice < 1 || choice > 9 || board[row][col] == 'X' || board[row][col] == 'O') {
+    if (choice < 1 || choice > 9 || board[row][col] == 'X' || board[row][col] == 'O') 
+    {
         return false;
-    } else {
+    } else 
+    {
         board[row][col] = mark;
         return true;
     }
@@ -40,7 +52,8 @@ bool placeMark(int choice, char mark) {
 
 bool checkWin(char mark) {
     // Check rows, columns, and diagonals
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++) 
+    {
         if ((board[i][0] == mark && board[i][1] == mark && board[i][2] == mark) ||
             (board[0][i] == mark && board[1][i] == mark && board[2][i] == mark))
             return true;
@@ -67,7 +80,8 @@ int main() {
 
     cout << "Tic Tac Toe Game (2 Players)\n";
 
-    while (!game_over) {
+    while (!game_over) 
+    {
         printBoard();
         cout << "Player " << player << ", enter your move (1-9): ";
         cin >> choice;
@@ -76,16 +90,20 @@ int main() {
             cout << "Invalid move! Try again.\n";
             continue;
         }
-
-        if (checkWin(player)) {
+        
+        if (checkWin(player)) 
+        {
             printBoard();
             cout << "Player " << player << " wins!\n";
             game_over = true;
-        } else if (checkDraw()) {
+        } else if (checkDraw()) 
+        {
             printBoard();
             cout << "It's a draw!\n";
             game_over = true;
-        } else {
+        } 
+        else 
+        {
             player = (player == 'X') ? 'O' : 'X';
         }
     }
